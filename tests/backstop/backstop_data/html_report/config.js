477cf3a7e81623427c50e917e79e8392f96712a0
Anton Novikov <novikov@tochkak.ru>
2017-09-18 18:31:40 +0300

    let's the fun begin

---
+++ tests/backstop/backstop_data/html_report/config.js
@@ -0,0 +1,181 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_wrapper_0_phone.png",
+        "test": "../bitmaps_test/20170918-181850/backstop_default_BackstopJS_Homepage_0_wrapper_0_phone.png",
+        "selector": ".wrapper",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_wrapper_0_phone.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_wrapper_1_tablet.png",
+        "test": "../bitmaps_test/20170918-181850/backstop_default_BackstopJS_Homepage_0_wrapper_1_tablet.png",
+        "selector": ".wrapper",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_wrapper_1_tablet.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_wrapper_2_macbook.png",
+        "test": "../bitmaps_test/20170918-181850/backstop_default_BackstopJS_Homepage_0_wrapper_2_macbook.png",
+        "selector": ".wrapper",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_wrapper_2_macbook.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_wrapper_3_bigscreen.png",
+        "test": "../bitmaps_test/20170918-181850/backstop_default_BackstopJS_Homepage_0_wrapper_3_bigscreen.png",
+        "selector": ".wrapper",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_wrapper_3_bigscreen.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "getDiffImage": null
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Mayak_Homepage_0_wrapper_0_phone.png",
+        "test": "../bitmaps_test/20170918-181850/backstop_default_Mayak_Homepage_0_wrapper_0_phone.png",
+        "selector": ".wrapper",
+        "fileName": "backstop_default_Mayak_Homepage_0_wrapper_0_phone.png",
+        "label": "Mayak Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.77",
+          "analysisTime": 219,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170918-181850/failed_diff_backstop_default_Mayak_Homepage_0_wrapper_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Mayak_Homepage_0_wrapper_1_tablet.png",
+        "test": "../bitmaps_test/20170918-181850/backstop_default_Mayak_Homepage_0_wrapper_1_tablet.png",
+        "selector": ".wrapper",
+        "fileName": "backstop_default_Mayak_Homepage_0_wrapper_1_tablet.png",
+        "label": "Mayak Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.85",
+          "analysisTime": 446,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170918-181850/failed_diff_backstop_default_Mayak_Homepage_0_wrapper_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Mayak_Homepage_0_wrapper_2_macbook.png",
+        "test": "../bitmaps_test/20170918-181850/backstop_default_Mayak_Homepage_0_wrapper_2_macbook.png",
+        "selector": ".wrapper",
+        "fileName": "backstop_default_Mayak_Homepage_0_wrapper_2_macbook.png",
+        "label": "Mayak Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.84",
+          "analysisTime": 699,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170918-181850/failed_diff_backstop_default_Mayak_Homepage_0_wrapper_2_macbook.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Mayak_Homepage_0_wrapper_3_bigscreen.png",
+        "test": "../bitmaps_test/20170918-181850/backstop_default_Mayak_Homepage_0_wrapper_3_bigscreen.png",
+        "selector": ".wrapper",
+        "fileName": "backstop_default_Mayak_Homepage_0_wrapper_3_bigscreen.png",
+        "label": "Mayak Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "1.20",
+          "analysisTime": 753,
+          "getDiffImage": null
+        },
+        "diffImage": "../bitmaps_test/20170918-181850/failed_diff_backstop_default_Mayak_Homepage_0_wrapper_3_bigscreen.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
